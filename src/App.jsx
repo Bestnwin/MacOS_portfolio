8535fc9395d137171660173acf299b5c968c8552
Akshit Sharma <akshitgamerz018@gmail.com>
2025-12-26 18:16:17 +0000

    implement welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navbar from '#components/Navbar.jsx'
+import { Navbar,Welcome } from './components/index'
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Welcome />
     </main>
   )
 }
