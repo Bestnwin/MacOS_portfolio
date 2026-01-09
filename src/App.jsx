3beb5f1dbc86c2835a83555a122006f2f96c1679
Akshit Sharma <akshitgamerz018@gmail.com>
2026-01-09 07:09:34 +0000

    implement higher order component for managing window states and add the first window - terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import { Navbar,Welcome,Dock } from './components/index'
+import { gsap } from 'gsap'
+import Draggable from 'gsap/Draggable';
+import { Terminal } from '#windows';
+
+//import { Terminal } from './windows/index';
+gsap.registerPlugin(Draggable);
+
 
 
 const App = () => {
@@ -8,7 +15,7 @@ const App = () => {
       <Navbar />
       <Welcome />
       <Dock />
-   
+      <Terminal />
     </main>
   )
 }
